a1a579b1002c2219f79c2f01fc04b1894e4dbe25
Truong20022003 <quangtruong20022003@gmail.com>
2024-06-07 00:42:28 +0700

    database

--- app/src/main/java/com/example/appdatcomtam/Login/SplashScreen.kt
+++ app/src/main/java/com/example/appdatcomtam/Login/SplashScreen.kt
@@ -1,4 +1,47 @@
 package com.example.appdatcomtam.Login
 
-class SplashScreen {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import com.example.appdatcomtam.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+                WelcomeScreen(onContinueClicked = {  })
+
+        }
+    }
+}
+
+@Composable
+fun WelcomeScreen(onContinueClicked: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logoimages),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+        }
+    }
 }
--- app/src/main/java/com/example/appdatcomtam/Model/Model.kt
+++ app/src/main/java/com/example/appdatcomtam/Model/Model.kt
@@ -1,53 +1,107 @@
 package com.example.appdatcomtam.Model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+
 @Entity
 data class LoaiMonAn(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val tenLoaiMonAn: String
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "tenLoaiMonAn") var tenLoaiMonAn: String?,
+
 )
+
+
+//@Entity
+//data class LoaiMonAn(
+//    @PrimaryKey(autoGenerate = true) val id: Int,
+//    val tenLoaiMonAn: String
+//)
+//
 
 @Entity
 data class MonAn(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val idLoaiMonAn: Int,
-    val gia: Double,
-    val tenMonAn: String
-)
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "idLoaiMonAn") var idLoaiMonAn: String?,
+    @ColumnInfo(name = "gia") var gia: Double ,
+    @ColumnInfo(name = "tenMonAn") var tenMonAn: String?,
+    )
+//@Entity
+//data class MonAn(
+//    @PrimaryKey(autoGenerate = true) val id: Int,
+//    val idLoaiMonAn: Int,
+//    val gia: Double,
+//    val tenMonAn: String
+//)
+//
 
 @Entity
 data class User(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val tenDangNhap: String,
-    val matKhau: String,
-    val diaChi: String
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "tenDangNhap") var tenDangNhap: String?,
+    @ColumnInfo(name = "matKhau") var matKhau: String?,
+    @ColumnInfo(name = "diaChi") var diaChi: String?,
 )
-
+//@Entity
+//data class User(
+//    @PrimaryKey(autoGenerate = true) val id: Int,
+//    val tenDangNhap: String,
+//    val matKhau: String,
+//    val diaChi: String
+//)
+//
 @Entity
 data class GioHang(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val idUser: Int,
-    val idMonAn: Int,
-    val soLuong: Int
-)
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "idUser") var idUser: Int ,
+    @ColumnInfo(name = "idMonAn") var idMonAn: Int ,
+    @ColumnInfo(name = "soLuong") var soLuong: Int ,
+
+    )
+
+//@Entity
+//data class GioHang(
+//    @PrimaryKey(autoGenerate = true) val id: Int,
+//    val idUser: Int,
+//    val idMonAn: Int,
+//    val soLuong: Int
+//)
+//
 
 @Entity
 data class DonHang(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val idUser: Int,
-    val ngayDatHang: String,
-    val tongTien: Double,
-    val trangThai: String
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "idUser") var idUser: Int ,
+    @ColumnInfo(name = "ngayDatHang") var ngayDatHang: String?,
+    @ColumnInfo(name = "tongTien") var tongTien: Double ,
+    @ColumnInfo(name = "trangThai") var trangThai: String?,
 )
+//@Entity
+//data class DonHang(
+//    @PrimaryKey(autoGenerate = true) val id: Int,
+//    val idUser: Int,
+//    val ngayDatHang: String,
+//    val tongTien: Double,
+//    val trangThai: String
+//)
+//
 
 @Entity
 data class ChiTietDonHang(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val maDonHang: Int,
-    val maSanPham: Int,
-    val soLuong: Int,
-    val donGia: Double,
-    val thanhTien: Double
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    @ColumnInfo(name = "maDonHang") var maDonHang: Int ,
+    @ColumnInfo(name = "maSanPham") var maSanPham: Int ,
+    @ColumnInfo(name = "soLuong") var soLuong: Int ,
+    @ColumnInfo(name = "donGia") var donGia: Double ,
+    @ColumnInfo(name = "thanhTien") var thanhTien: Double ,
 )
+//@Entity
+//data class ChiTietDonHang(
+//    @PrimaryKey(autoGenerate = true) val id: Int,
+//    val maDonHang: Int,
+//    val maSanPham: Int,
+//    val soLuong: Int,
+//    val donGia: Double,
+//    val thanhTien: Double
+//)
