39dbbd5f1849fa736e166289cba08626c320b176
Doan379a <doan379a@gmail.com>
2024-06-06 22:27:05 +0700

    giao dien them sua xoa mon an

---
+++ app/src/main/java/com/example/appdatcomtam/Quanly/Sua/SuaMonAnActivity.kt
@@ -0,0 +1,26 @@
+package com.example.appdatcomtam.Quanly.Sua
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+
+
+
+class SuaMonAnActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CompositionLocalProvider(LocalViewModelStoreOwner provides this) {
+                val viewModelSua: SuaMonAnViewModel = viewModel()
+                SuaMonAnScreen(viewModelSua)
+            }
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/appdatcomtam/Quanly/Sua/SuaMonAnViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.appdatcomtam.Quanly.Sua
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class SuaMonAnViewModel :ViewModel(){
+    var gia by mutableStateOf("")
+    var tenMonAn by mutableStateOf("")
+    var imageUri by mutableStateOf<Uri?>(null)
+
+    fun onClickAdd(context: Context) {
+        if (gia.isEmpty() || tenMonAn.isEmpty()) {
+            Toast.makeText(context, "không được để trống", Toast.LENGTH_SHORT).show()
+        } else {
+            try {
+                val giaValue = gia.toDouble()
+                Log.d(
+                    "ThemMonAn",
+                    " Giá: $gia, Tên món ăn: $tenMonAn"
+                )
+                Toast.makeText(context, "Thêm món ăn thành công", Toast.LENGTH_SHORT).show()
+            } catch (e: NumberFormatException) {
+                Toast.makeText(context, "Giá phải là số", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
